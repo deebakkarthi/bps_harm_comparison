3e8b65694f7a3990d11387eb7f49ad6e6984b116
Deebakkarthi C R <dbk@deebakkarthi.com>
2026-05-26 18:06:34 -0400

    Compare commong

--- scripts/in_harm_no_restriction_not_in_bps_proven.sh
+++ scripts/in_harm_no_restriction_not_in_bps_proven.sh
@@ -3,5 +3,5 @@
 stages=("frontend.csv" "id_stage.csv" "issue_stage.csv" "ex_stage.csv" "commit_stage.csv")
 
 for stage in "${stages[@]}"; do
-	./compare.py "./csv/harm_no_restriction/${stage}" "./csv/bps/${stage}" --common --category
+	./compare.py "./csv/harm_no_restriction/${stage}" "./csv/bps/${stage}" --category | grep proven
 done
